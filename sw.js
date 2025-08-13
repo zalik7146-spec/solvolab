c618e97ee0ea01ea6ce8527838d40aec2909a941
zalik7146-spec <zalik7146@gmail.com>
2025-08-13 14:11:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE = 'solvo-v1';
 const ASSETS = [
-  './', './index.html', './app.html', './css/styles.css',
-  './manifest.webmanifest', './assets/icon.svg'
+  './', './index.html', './app.html',
+  './css/styles.css', './manifest.webmanifest', './assets/icon.svg'
 ];
 
 self.addEventListener('install', e => {
@@ -9,11 +9,15 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(resp => resp || fetch(e
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(resp => resp || fetch(e.request))
+  );
+});
