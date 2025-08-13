a251bf53dbef177c1cb7d80d7c60bc54f3151bdd
zalik7146-spec <zalik7146@gmail.com>
2025-08-13 14:14:44 +0300

    Create common.js

---
+++ js/common.js
@@ -0,0 +1,11 @@
+window.$id = (id) => document.getElementById(id);
+window.uid = () => Math.random().toString(36).slice(2,10);
+window.today = () => DB.todayKey();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const path = location.pathname.split('/').pop();
+  document.querySelectorAll('.tab').forEach(a=>{
+    const href = a.getAttribute('href');
+    if (href === path) a.classList.add('active');
+  });
+});
